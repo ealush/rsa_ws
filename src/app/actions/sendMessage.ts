527e3f8436b3679bcc69f233e831b0946744c3d0
Evyatar Alush <ealush@meta.com>
2026-02-04 18:28:59 +0800

    module_07_sendMessage

--- src/app/actions/sendMessage.ts
+++ src/app/actions/sendMessage.ts
@@ -2,4 +2,23 @@
 
 import { prisma } from "../db/prismaClient";
 
-export async function sendMessage(formData: FormData) {}
+export async function sendMessage(formData: FormData) {
+  const id = formData.get("id") as string;
+  const content = formData.get("content") as string;
+
+  const message = await prisma.message.create({
+    data: {
+      content,
+      contact: {
+        connect: {
+          id: parseInt(id),
+        },
+      },
+    },
+  });
+
+  return {
+    ...message,
+    timestamp: message.timestamp.toISOString(),
+  };
+}
--- src/app/components/Pager.tsx
+++ src/app/components/Pager.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import React, {
-  useState,
-  useEffect,
-  FormEvent,
-  ChangeEvent,
-  useCallback,
-} from "react";
+import React, { useState, useEffect, useCallback, useActionState } from "react";
 import styles from "./pager.module.css";
 
 import clsx from "clsx";
+import { sendMessage } from "../actions/sendMessage";
 
 interface Message {
   id: number;
@@ -26,19 +21,22 @@ interface PagerProps {
 
 export default function Pager({ contactId }: PagerProps) {
   const [messages, setMessages] = useState<Messages>([]);
-  const [inputValue, setInputValue] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [, formAction, isPending] = useActionState(send, null);
+
+  async function send(_: unknown, formData: FormData) {
+    const message = await sendMessage(formData);
+
+    setMessages((prev) => [message, ...prev] as Messages);
+  }
 
   const fetchMessages = useCallback(
     async function fetchMessages() {
-      setIsLoading(true);
       const response = await fetch(`/api/contacts/${contactId}/message`);
 
       const data: Messages = await response.json();
       setMessages(data);
-      setIsLoading(false);
     },
-    [contactId]
+    [contactId],
   );
 
   useEffect(() => {
@@ -46,41 +44,8 @@ export default function Pager({ contactId }: PagerProps) {
       fetchMessages();
     } else {
       setMessages([]);
-      setIsLoading(false);
     }
   }, [contactId, fetchMessages]);
-
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSendMessage = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!inputValue.trim()) return;
-
-    const response = await fetch(`/api/contacts/${contactId}/message`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: contactId,
-        content: inputValue,
-      }),
-    });
-
-    if (!response.ok) {
-      const errorData = await response
-        .json()
-        .catch(() => ({ error: "Unknown error" }));
-      throw new Error(
-        errorData.error || `HTTP error! status: ${response.status}`
-      );
-    }
-
-    setInputValue("");
-    fetchMessages();
-  };
 
   return (
     <div className={styles.pagerContainer}>
@@ -91,22 +56,19 @@ export default function Pager({ contactId }: PagerProps) {
           <MessageItem key={msg.id} msg={msg} />
         ))}
       </div>
-      <form onSubmit={handleSendMessage} className={styles.messageForm}>
+      <form className={styles.messageForm} action={formAction}>
         <input type="hidden" name="id" value={contactId} />
         <input
           type="text"
-          value={inputValue}
-          onChange={handleInputChange}
           placeholder="Type your message..."
           className={styles.messageInput}
           aria-label="New message input"
-          disabled={isLoading}
           name="content"
         />
         <button
           type="submit"
           className={styles.sendButton}
-          disabled={!inputValue.trim() || isLoading}
+          disabled={isPending}
         >
           Send
         </button>
