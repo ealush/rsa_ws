bbd52280040748cd486ea0982d4d6e128eb721cc
Evyatar Alush <ealush@meta.com>
2026-02-04 17:29:37 +0800

    module_02_button_delete

--- src/app/components/ButtonRemoveContact.tsx
+++ src/app/components/ButtonRemoveContact.tsx
@@ -1,35 +1,24 @@
-"use client";
-
 import { FaTrash } from "react-icons/fa";
-import { useRouter } from "next/navigation";
 import styles from "./Contact.module.css";
+import { prisma } from "../db/prismaClient";
+import { revalidatePath } from "next/cache";
 
 export function ButtonRemoveContact({ id }: { id: number }) {
-  const router = useRouter();
-
   return (
-    <button
-      className={styles.actionButton}
-      onClick={() => removeContact(id)}
-      title="Remove Contact"
+    <form
+      action={async () => {
+        "use server";
+
+        await prisma.contact.delete({
+          where: { id },
+        });
+
+        revalidatePath("/");
+      }}
     >
-      <FaTrash />
-    </button>
+      <button className={styles.actionButton} title="Remove Contact">
+        <FaTrash />
+      </button>
+    </form>
   );
-
-  async function removeContact(contactId: number) {
-    try {
-      const response = await fetch(`/api/contacts/${contactId}`, {
-        method: "DELETE",
-      });
-
-      if (response.ok) {
-        // Refresh the page to show updated contact list
-        router.refresh();
-        router.push("/");
-      }
-    } catch (error) {
-      console.error("Error removing contact:", error);
-    }
-  }
 }
