e1845e72f1ed6ad9bd21a57019235cdb289e70a3
Evyatar Alush <ealush@meta.com>
2026-02-25 17:16:31 +0200

    lint

--- src/app/validation/chronoVestSuite.ts
+++ src/app/validation/chronoVestSuite.ts
@@ -15,71 +15,74 @@ const ChronoSchema = enforce.shape({
 export type ChronosSchemaType = typeof ChronoSchema.infer;
 
 export const chronoVestSuite = create((data = INITIAL_CHRONO_FORM) => {
-
   group("mission", () => {
     test("travelerName", "Traveler name is required", () => {
       enforce(data.travelerName).isNotBlank();
     });
-    
-    test("travelerName", "Traveler name must be longer than 3 characters", () => {
-      enforce(data.travelerName).longerThan(3);
-    });
-    
+
+    test(
+      "travelerName",
+      "Traveler name must be longer than 3 characters",
+      () => {
+        enforce(data.travelerName).longerThan(3);
+      },
+    );
+
     test("mission", "Mission is required", () => {
       enforce(data.mission).isNotBlank();
     });
-    
+
     test("mission", "Mission must be longer than 3 words", () => {
       enforce(data.mission).matches(/\b\w{3,}\b/g);
     });
-    
+
     test("birthYear", "Birth year is required", () => {
       enforce(data.birthYear).isNotBlank();
     });
-    
+
     test("birthYear", "Birth year must be a number", () => {
       enforce(data.birthYear).isNumeric();
     });
-  })
+  });
 
   group("calibration", () => {
     test("destinationYear", "Destination year is required", () => {
       enforce(data.destinationYear).isNotBlank();
     });
-    
+
     test("destinationYear", "Destination year must be a number", () => {
       enforce(data.destinationYear).isNumeric();
     });
 
     test("destinationYear", "Warning: Avoid meeting yourself!", () => {
-    warn();
+      warn();
 
-    enforce(data.destinationYear).isNotBetween(
-      Number(data.birthYear),
-      new Date().getFullYear(),
-    );
+      enforce(data.destinationYear).isNotBetween(
+        Number(data.birthYear),
+        new Date().getFullYear(),
+      );
     });
-    
-    
-    memo(() => {    
-      test("destinationYear", debounce(async () => {
-        const warn = useWarn();
-        const congestion = await getCongestion(Number(data.destinationYear));
-        
-        const message = `${congestion.level}: ${congestion.icon} ${congestion.label}`;
-        
-        enforce(congestion.level)
-        .message(message)
-        .notEquals("HEAVY");
 
-        warn();
-        
-        enforce(congestion.level)
-        .message(`WARNING: ${message}`)
-        .notMatches(/MODERATE|LIGHT/);
-      }, 1000));
-    }, [data.destinationYear])
-    
+    memo(() => {
+      test(
+        "destinationYear",
+        debounce(async () => {
+          const warn = useWarn();
+          const congestion = await getCongestion(Number(data.destinationYear));
+
+          const message = `${congestion.level}: ${congestion.icon} ${congestion.label}`;
+
+          enforce(congestion.level).message(message).notEquals("HEAVY");
+
+          warn();
+
+          enforce(congestion.level)
+            .message(`WARNING: ${message}`)
+            .notMatches(/MODERATE|LIGHT/);
+        }, 1000),
+      );
+    }, [data.destinationYear]);
+
     test("plutoniumCores", "Plutonium cores must be a number", () => {
       enforce(data.plutoniumCores).isNumeric();
     });
